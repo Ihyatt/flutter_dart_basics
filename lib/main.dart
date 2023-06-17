045ecffb0a72e1e8d3ea459aecc638ee21376d93
Ihyatt <inas.raheema@gmail.com>
2023-06-16 21:34:47 -0700

    visuals corrected

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,44 @@ import 'package:flutter/material.dart';
 
 void main() {
   return runApp(
-    const MaterialApp(
+    MaterialApp(
       home: Scaffold(
-        body: Container()
-      )
+
+          body: Container(
+          color: const Color(0XFFCF9393),
+          child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget> [
+
+              Image.asset(
+                  'assets/images/quiz-logo.png',
+                width: 200,
+              ),
+              const SizedBox(height: 20),
+              const Text(
+                'Learn FLutter the fun way!',
+                style: TextStyle(
+                  color: Color(0XFFFFEEEE),
+                    fontSize: 25
+                ),
+              ),
+              const SizedBox(height: 20),
+              const TextButton(
+                  onPressed: null,
+                  child: Text(
+                      'Start quiz',
+                      style: TextStyle(
+                        color: Color(0XFFFFEEEE),
+                          fontSize: 15
+                      ),
+                  ),
+              ),
+          ],
+          ),
+        ),
+      ),
+      ),
     )
   );
 
